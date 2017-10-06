85212bd83a70678ff29d1b553ca7df8b84b83ecb
luke filewalker <loeken@internetz.me>
2017-10-06 13:39:29 +0200

    added php class

---
+++ cryptocompareApi.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loeken
+ * Date: 10/6/17
+ * Time: 12:35 PM
+ */
+
+class CryptocompareApi
+{
+
+    /**
+     * retrieves an array of objects listing all available api endpoints
+     */
+    public function availableCalls() {
+
+    }
+
+    /**
+     * will send request to api endpoint
+     */
+    private function getRequest() {
+
+    }
+}
