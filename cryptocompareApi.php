e89337c8b703450a29710c3e21f64822d4a1dde8
luke filewalker <loeken@internetz.me>
2017-10-06 14:39:12 +0200

    test 2

--- cryptocompareApi.php
+++ cryptocompareApi.php
@@ -1,186 +1,73 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: user
- * Date: 9/6/17
- * Time: 8:05 AM
+ * User: loeken
+ * Date: 10/6/17
+ * Time: 12:35 PM
  */
 
-namespace Webpwnize\Modules\Dashboard\Controllers;
-
-
-use Webpwnize\AnsibleHelper;
-use Asm\Ansible\Ansible;
-use Webpwnize\Icinga2;
-use Webpwnize\Models\Server;
-
-class TestController extends DashboardControllerBase
+class CryptocompareApi
 {
-    public function apiAction() {
-        echo "here we go";
-        $cryptocompareApi = new \CryptocompareApi();
-        $r = $cryptocompareApi->availableCalls();
-        print_r($r);
-        echo "end";
-        die();
-    }
-    public function icingaaddAction() {
-        $icinga = new Icinga2();
-        $servers = Server::find("active=1");
+    /**
+     * @var string publicEndpoint applies to all requests that do not need a session key to work
+     */
+    public $publicEndpoint = "https://min-api.cryptocompare.com";
 
-        $i = 0;
-        echo count($servers);
-        foreach ($servers as $i => $server ) {
-            echo "<br>"  . $server->getServername() . " -- ";
-            if ($server->getInternalIp() != $icinga->getHost($server->getInternalName())->address ) {
-                echo "doesnt match";
-            }
+    /**
+     * @var string privateEndpoint applies to all requests that do need a session key to work
+     */
+    public $privateEndpoint ="https://min-api.cryptocompare.com/api/data";
+
+    /**
+     * @var array contains strings with errors
+     */
+    public $errorMessages = array();
+
+    /**
+     * retrieves an array of objects listing all available api endpoints
+     */
+    public function availableCalls() {
+        return $this->getRequest("private","/");
+    }
+    private function getErrorMessages() {
+
+    }
+    /**
+     * will send request to api endpoint
+     */
+    private function getRequest($type = "public", $action = "") {
+        if ($action == "" ) {
+            $this->errorMessages[] = "no action submitted";
+            return false;
         }
-        die();
-    }
-    public function icingatestAction() {
-        $icinga = new Icinga2();
-        $servers = Server::find();
-        $i = 0;
-        echo count($servers);
-        foreach ($servers as $server ) {
-            echo "<br>" . $i . " - "  . $server->getServername();
-            if ($server->getInternalName() == "" ) {
-
-                $r1 = $this->getInternalHostname($server);
-
-                if ($r1 != false ) {
-                    $server->setInternalName($r1);
-                    $server->save();
-                }
-                else {
-                    if (fnmatch("*ccc-streamer*", $server->getServername())) {
-                        $r1 = $server->getServername() . $server->getSuffix() . ".ccc-stream.a6.internal.cloudapp.net";
-                        $server->setInternalName($r1);
-                        $server->save();
-                    } elseif (fnmatch("*ccc-api*", $server->getServername())) {
-                        $r1 = $server->getServername() . $server->getSuffix() . ".ccc-api.a3.internal.cloudapp.net";
-                        $server->setInternalName($r1);
-                        $server->save();
-                    }  elseif (fnmatch("*ccc-web*", $server->getServername())) {
-                        $r1 = $server->getServername() . $server->getSuffix() . ".ccc-web.cloudapp.net";
-                        $server->setInternalName($r1);
-                        $server->save();
-                    }   elseif (fnmatch("*ccc-widgets*", $server->getServername())) {
-                        $r1 = $server->getServername() . $server->getSuffix() . ".ccc-widgets.a10.internal.cloudapp.net";
-                        $server->setInternalName($r1);
-                        $server->save();
-                    } else {
-                        $r1 = $server->getServername() . $server->getSuffix() . ".cloudapp.net";
-                    }
-                }
-            }
-            else {
-                $r1 = $server->getInternalName();
-            }
-            echo $r1;
-
-            //$r = $icinga->getHost($r1);
-            //print_r($r);
-
-            echo "<br>";
-            $i++;
+        if ($type == "public" ) {
+            $uri = $this->publicEndpoint . $action;
         }
-        die();
-    }
-    public function bruteforceipsAction() {
-        $servers = Server::find();
-        foreach ($servers as $server ) {
-            if ($server->getInternalIp() == "" ) {
-                $hostname = $this->hostnamebase($server);
-
-                $i = 1;
-                while ($i <= 10) {
-                    $thost = $hostname . "a" . $i . ".internal.cloudapp.net";
-                    $lookup = gethostbyname($thost);
-                    if ($lookup != $thost) {
-                        echo "FOUND: " . $lookup;
-                        $server->setInternalIp($lookup);
-                        $server->save();
-                        break;
-                    }
-                    $i++;
-                }
-            }
-            else {
-                $hostname = "local ip: " . $server->getInternalIp();
-            }
-            echo $server->getServername() . " - " .  $hostname . "<br>";
-
-
-            if ($server->getPublicIp() == "" ) {
-                $_server = $server->getServername();
-                if ($server->getSuffix() != "" ) {
-                    $_server = $_server . $server->getSuffix();
-                }
-                $lookup = gethostbyname($_server . ".cloudapp.net");
-                if ($lookup != $_server . ".cloudapp.net" ) {
-                    $server->setPublicIp($lookup);
-                    $server->save();
-                }
-
-                $lookup = gethostbyname($_server . ".westeurope.cloudapp.azure.com");
-                if ($lookup != $_server . ".westeurope.cloudapp.azure.com" ) {
-                    $server->setPublicIp($lookup);
-                    $server->save();
-                }
-            }
-        }
-    }
-    public function hostnamebase($server) {
-
-        $t = $server->getServername();
-        $t = $t . ".";
-        $t = $t . $server->getServername();
-
-        if ($server->getInternalIp() == "" ) {
-            $t = $t . ".";
-            $t = $t . $server->getServername();
-            if ($server->getSuffix() != "" ) {
-                $t = $t . $server->getSuffix();
-            }
+        elseif ($type == "private" ) {
+            $uri = $this->privateEndpoint . $action;
         }
         else {
-            if ($server->getSuffix() != "" ) {
-                $t = $t . $server->getSuffix();
-            }
+            $this->errorMessages[] = "invalid type specified";
+            return false;
         }
-        $t = $t . ".";
-        return $t;
-    }
-    public function getInternalHostname($server) {
-        $hostname = $this->hostnamebase($server);
+        try {
 
-        $i = 1;
-        while ($i <= 10) {
-            $thost = $hostname . "a" . $i . ".internal.cloudapp.net";
+            $client = new \GuzzleHttp\Client(['verify' => false]);
 
-            $lookup = gethostbyname($thost);
+            $res = $client->request('GET', $uri);
+            $this->statusCode = $res->getStatusCode();
+            print_r($this->statusCode);
+            // "200"
+            $this->header = $res->getHeader('content-type');
+            // 'application/json; charset=utf8'
+            $this->body = $res->getBody()->getContents();
+            print_r($this->body);
 
-            if ($lookup != $thost) {
-                return $thost;
-            }
-            $i++;
+            return json_decode($this->body);
         }
-        return false;
-    }
-
-    public function getInternalIp($server) {
-        $hostname = $this->hostnamebase($server);
-        $i = 1;
-        while ($i <= 10) {
-            $thost = $hostname . "a" . $i . ".internal.cloudapp.net";
-            $lookup = gethostbyname($thost);
-
-            if ($lookup != $thost) {
-                return $lookup;
-            }
-            $i++;
+        catch (\Exception $e) {
+            return json_decode($this->body);
+            //echo 'Exception abgefangen: ', $e->getMessage(), "\n";
         }
     }
 }
